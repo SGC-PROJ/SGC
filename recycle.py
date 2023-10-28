49cf2de871163f043a005fc79c66276cd83f8732
chopin <tmdxor2580@naver.com>
2023-10-28 21:57:45 +0900

    Update recycle.py

--- recycle.py
+++ recycle.py
@@ -1,9 +1,18 @@
 import time
 import serial
 import os
-import realserver
 import socket
+import pygame
 
+plastic = "sgc_sound/plastic.wav"
+complete = "sgc_sound/complete.wav"
+change = "sgc_sound/change.wav"
+glass = "sgc_sound/glass.wav"
+metal = "sgc_sound/metal.wav"
+general = "sgc_sound/general.wav"
+SGC = "sgc_sound/SGC.wav"
+pygame.init()
+pygame.display.set_mode((1, 1))
 ser = serial.Serial("../../../dev/ttyACM0", 57600)
 os.environ['DISPLAY'] = ':0'
 server_ip = '192.168.239.42'
@@ -45,18 +54,40 @@ def recycle(detect):
     if detect == 0 or detect == 1 or detect == 2 or detect == 3 or detect == 7 or detect == 8 or detect == 9 or detect == 10: #일쓰
         ser.write(b't')
         ti = 't'
+        pygame.mixer.music.load(general)
+        pygame.mixer.music.play()    
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+        pygame.quit()
         time.sleep(5)
+
     elif detect == 6: #플라스틱
         ser.write(b'p')
         ti = 'p'
+        pygame.mixer.music.load(plastic)
+        pygame.mixer.music.play()    
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+        pygame.quit()
         time.sleep(5)
     elif detect == 4: #철,캔
         ser.write(b'm')
         ti = 'm'
+        pygame.mixer.music.load(metal)
+        pygame.mixer.music.play()    
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+        pygame.quit()
         time.sleep(5)
+        
     elif detect == 5: #유리
         ser.write(b'g')
         ti = 'g'
+        pygame.mixer.music.load(glass)
+        pygame.mixer.music.play()    
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+        pygame.quit()
         time.sleep(5)
 
     line = ser.readline()  # <3
@@ -68,11 +99,16 @@ def recycle(detect):
     distance=int(data[0])
     time.sleep(5)
 
-    if distance < 40:
+    if distance < 35:
         
     
         message = ti
         client_socket.send(message.encode())
+        pygame.mixer.music.load(complete)
+        pygame.mixer.music.play()    
+        while pygame.mixer.music.get_busy():
+                    pygame.time.Clock().tick(10)
+        
 
         while True:
                 # message=input()
@@ -81,6 +117,11 @@ def recycle(detect):
                 data = client_socket.recv(1024)
                 if data.decode() == 'end':
                     print("서버 응답:", data.decode())
+                    pygame.mixer.music.load(complete)
+                    pygame.mixer.music.play()    
+                    while pygame.mixer.music.get_busy():
+                        pygame.time.Clock().tick(10)
+                    pygame.quit()
                     break
                     time.sleep(1)
         
