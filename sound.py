9a991d411d6749f16f7a9d0e809f6debafe4d8fc
KyoungHoLee-github <101320758+KyoungHoLee-github@users.noreply.github.com>
2023-11-08 14:24:10 +0900

    sound.py

--- sound.py
+++ sound.py
@@ -3,20 +3,20 @@ import serial
 
 ser = serial.Serial("../../../dev/ttyACM0", 57600)
 
+
 def sound():
     pygame.init()
 
     SGC = "sgc_sound/SGC.wav"
-    ser.write(b'b')
+    ser.write(b"b")
 
-    line = ser.readline().decode('utf-8').strip()
+    line = ser.readline().decode("utf-8").strip()
 
-    if(line == 'y'):
+    if line == "y":
         pygame.display.set_mode((1, 1))
         pygame.mixer.music.load(SGC)
-        pygame.mixer.music.play()    
+        pygame.mixer.music.play()
         while pygame.mixer.music.get_busy():
             pygame.time.Clock().tick(10)
-        pygame.quit()
     else:
         return
