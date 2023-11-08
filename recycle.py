9ff432341bc953dc029c3e256ec342b50d73c174
KyoungHoLee-github <101320758+KyoungHoLee-github@users.noreply.github.com>
2023-11-08 14:25:42 +0900

    recycle.py

--- recycle.py
+++ recycle.py
@@ -1,7 +1,6 @@
 import time
 import serial
 import os
-import socket
 import pygame
 
 plastic = "sgc_sound/plastic.wav"
@@ -14,171 +13,109 @@ SGC = "sgc_sound/SGC.wav"
 pygame.init()
 pygame.display.set_mode((1, 1))
 ser = serial.Serial("../../../dev/ttyACM0", 57600)
-os.environ['DISPLAY'] = ':0'
-server_ip = '192.168.239.42'
-server_port = 12345
+os.environ["DISPLAY"] = ":0"
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((server_ip, server_port))
-server_socket.listen(1)
-print("서버 대기 중...")
-client_socket, client_address = server_socket.accept()
-print("클라이언트가 연결되었습니다:", client_address)
-def recycle(detect):
+def recycle(detect, client_socket):
     if detect == 0:
-        stuff = 'UNKNOwN'
+        stuff = "UNKNOwN"
     elif detect == 1:
-        stuff = 'General Trash'
+        stuff = "General Trash"
     elif detect == 2:
-        stuff = 'Paper'
+        stuff = "Paper"
     elif detect == 3:
-        stuff = 'Paper pack'
+        stuff = "Paper pack"
     elif detect == 4:
-        stuff = 'Metal'
+        stuff = "Metal"
     elif detect == 5:
-        stuff = 'Glass'
+        stuff = "Glass"
     elif detect == 6:
-        stuff = 'Plastic'
+        stuff = "Plastic"
     elif detect == 7:
-        stuff = 'Styrofoam'
+        stuff = "Styrofoam"
     elif detect == 8:
-        stuff = 'Plastic bag'
+        stuff = "Plastic bag"
     elif detect == 9:
-        stuff = 'Bettery'
+        stuff = "Bettery"
     elif detect == 10:
-        stuff = 'Clothing'
+        stuff = "Clothing"
     else:
-        stuff = 'UNKNOwN'
+        stuff = "UNKNOwN"
     print(detect)
-    print(f'detect = {detect}, stuff = {stuff}')
-    if detect == 0 or detect == 1 or detect == 2 or detect == 3 or detect == 7 or detect == 8 or detect == 9 or detect == 10: #일쓰
-        ser.write(b't')
-        ti = 't'
+    print(f"detect = {detect}, stuff = {stuff}")
+    if (
+        detect == 0
+        or detect == 1
+        or detect == 2
+        or detect == 3
+        or detect == 7
+        or detect == 8
+        or detect == 9
+        or detect == 10
+    ):  # 일쓰
+        ser.write(b"t")
+        ti = "t"
         pygame.mixer.music.load(general)
-        pygame.mixer.music.play()    
+        pygame.mixer.music.play()
         while pygame.mixer.music.get_busy():
             pygame.time.Clock().tick(10)
-        pygame.quit()
         time.sleep(5)
 
-    elif detect == 6: #플라스틱
-        ser.write(b'p')
-        ti = 'p'
+    elif detect == 6:  # 플라스틱
+        ser.write(b"p")
+        ti = "p"
         pygame.mixer.music.load(plastic)
-        pygame.mixer.music.play()    
+        pygame.mixer.music.play()
         while pygame.mixer.music.get_busy():
             pygame.time.Clock().tick(10)
-        pygame.quit()
-        time.sleep(5)
-    elif detect == 4: #철,캔
-        ser.write(b'm')
-        ti = 'm'
-        pygame.mixer.music.load(metal)
-        pygame.mixer.music.play()    
-        while pygame.mixer.music.get_busy():
-            pygame.time.Clock().tick(10)
-        pygame.quit()
         time.sleep(5)
         
-    elif detect == 5: #유리
-        ser.write(b'g')
-        ti = 'g'
-        pygame.mixer.music.load(glass)
-        pygame.mixer.music.play()    
+    elif detect == 4:  # 철,캔
+        ser.write(b"m")
+        ti = "m"
+        pygame.mixer.music.load(metal)
+        pygame.mixer.music.play()
         while pygame.mixer.music.get_busy():
             pygame.time.Clock().tick(10)
-        pygame.quit()
+        time.sleep(5)
+
+    elif detect == 5:  # 유리
+        ser.write(b"g")
+        ti = "g"
+        pygame.mixer.music.load(glass)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
         time.sleep(5)
 
     line = ser.readline()  # <3
     data = str(line)
-    data = data.split('\'')
+    data = data.split("'")
     data = str(data[1])
-    data = data.split('\\')
+    data = data.split("\\")
     print(data[0])
-    distance=int(data[0])
+    distance = int(data[0])
     time.sleep(5)
 
     if distance < 35:
-        
-    
         message = ti
         client_socket.send(message.encode())
         pygame.mixer.music.load(complete)
-        pygame.mixer.music.play()    
+        pygame.mixer.music.play()
         while pygame.mixer.music.get_busy():
-                    pygame.time.Clock().tick(10)
-        
+            pygame.time.Clock().tick(10)
 
         while True:
-                # message=input()
-                # client_socket.send(message.encode())
-        
-                data = client_socket.recv(1024)
-                if data.decode() == 'end':
-                    print("서버 응답:", data.decode())
-                    pygame.mixer.music.load(complete)
-                    pygame.mixer.music.play()    
-                    while pygame.mixer.music.get_busy():
-                        pygame.time.Clock().tick(10)
-                    pygame.quit()
-                    break
-                    time.sleep(1)
-        
-                print("서버 응답:", data.decode())
-        
+            # message=input()
+            # client_socket.send(message.encode())
 
-    '''
-    if detect == 0:
-        detect = 'UNKNOwN'
-    elif detect == 1:
-        detect = 'General Trash'
-    elif detect == 2:
-        detect = 'Paper'
-    elif detect == 3:
-        detect = 'Paper pack'
-    elif detect == 4:
-        detect = 'Metal'
-    elif detect == 5:
-        detect = 'Glass'
-    elif detect == 6:
-        detect = 'Plastic'
-    elif detect == 7:
-        detect = 'Styrofoam'
-    elif detect == 8:
-        detect = 'Plastic bag'
-    elif detect == 9:
-        detect = 'Bettery'
-    elif detect == 10:
-        detect = 'Clothing'
-    else:
-        detect = 'UNKNOwN'
-    print(detect)
-    
-    port = serial.Serial("/dev/ttyACM0", baudrate=9600, timeout=None)
-    if detect == 0: #Unknown
-        port.write(b'0')
-    elif detect ==1: #'General Trash'
-        port.write(b'1')
-    elif detect ==2: #'Paper'
-        port.write(b'2')
-    elif detect ==3: #'Paper pack'
-        port.write(b'3')
-    elif detect ==4:#'Metal'
-        port.write(b'4')
-    elif detect ==5:#'Glass'
-        port.write(b'5')
-    elif detect ==6:#'Plastic'
-        port.write(b'6')
-    elif detect ==7:#'Styrofoam'
-        port.write(b'7')
-    elif detect ==8:# 'Plastic bag'
-        port.write(b'8')
-    elif detect ==9:#'Bettery'
-        port.write(b'9')
-    elif detect ==10:#'Clothing'
-        port.write(b'10')
-    else:
-        port.write(b'0')
-        '''
-    # print(detect)
+            data = client_socket.recv(1024)
+            if data.decode() == "end":
+                print("서버 응답:", data.decode())
+                pygame.mixer.music.load(complete)
+                pygame.mixer.music.play()
+                while pygame.mixer.music.get_busy():
+                    pygame.time.Clock().tick(10)
+                break
+                time.sleep(1)
+
+            print("서버 응답:", data.decode())
